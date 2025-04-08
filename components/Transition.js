ba45c2974625143f7bf209a11cef79cabd6d77ab
Rango247on <aminul007.ais@gmail.com>
2025-04-08 20:13:21 +0600

    Testing repo-2

--- components/Transition.js
+++ components/Transition.js
@@ -33,7 +33,7 @@ const Transition = () => {
         exit="exit"
         transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
       ></motion.div>
-
+  {/* hello test  */}
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
         variants={transitionVariants}
